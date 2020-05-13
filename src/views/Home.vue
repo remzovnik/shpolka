4e84046b452a3b4df03f985caff31c75d98a8077
remzov <remzovnik@gmail.com>
2020-05-13 19:23:39 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Header msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+  },
+};
+</script>
