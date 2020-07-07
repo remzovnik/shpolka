138c67754b716dfe43e557354ba6aef043cc5070
remzov <remzovnik@gmail.com>
2020-07-07 10:15:49 +0300

    rrouter-view transition added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <MainHeader/>
-    <router-view/>
+    <transition :name="transitionName">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -12,5 +14,37 @@ export default {
   components: {
     MainHeader,
   },
+
+  data() {
+    return {
+      transitionName: null,
+    };
+  },
+
+  watch: {
+    $route(to) {
+      this.transitionName = !(['home', 'error'].includes(to.name)) ? 'fade' : null;
+    },
+  },
 };
 </script>
+
+
+<style>
+  .fade-enter-active {
+    transition: opacity ease-out 0.6s, transform ease-out .3s;
+  }
+
+  .fade-leave-active {
+    transition: opacity ease-in 0.5s
+  }
+
+  .fade-leave {
+    opacity: 0;
+  }
+
+  .fade-enter {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+</style>
